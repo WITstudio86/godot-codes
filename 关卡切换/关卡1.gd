ee45d2bc8013ef6d3fdc70a008855996edd6b9f7
WITstudio86 <1656800105@qq.com>
2025-09-22 22:16:06 +0800

    feat: 关卡切换

---
+++ 关卡切换/关卡1.gd
@@ -0,0 +1,16 @@
+extends Node2D
+class_name l1
+
+#@export var target_level : PackedScene
+@export var scence_path : String # 解决覆盖为 null 的情况
+
+func _ready() -> void:
+	if not scence_path:
+		# 或者兜底
+		scence_path = "res://关卡切换/关卡2.tscn"
+
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.is_action_pressed("primary action"):
+			get_tree().change_scene_to_file(scence_path)
+			print('切换 , target level:' , scence_path)
